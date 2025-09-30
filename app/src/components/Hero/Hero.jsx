a8f754689d18680a24086946add54f23c879f6ea
Gabriel Dossa <gabrielvinces2@gmail.com>
2025-09-30 18:09:17 -0300

    Updating site

--- app/src/components/Hero/Hero.jsx
+++ app/src/components/Hero/Hero.jsx
@@ -20,10 +20,10 @@ export default function Hero() {
             </p>
             <div style={{ display: "flex", gap: 12 }}>
               <div className="whatsBtn">
-                <Button icon={'../../imagens/icones/Home_branco.png'} link={'/servicos'} title='Nossos Serviços' ></Button>
+                <Button icon={'/imagens/icones/Home_branco.png'} link={'/servicos'} title='Nossos Serviços' ></Button>
               </div>
               <div className="whatsBtn">
-                <Button icon={'../../imagens/icones/whats.png'} link={'https://wa.me/+556530231011'} title='WhatsApp' ></Button>
+                <Button icon={'/imagens/icones/whats.png'} link={'https://wa.me/+556530231011'} title='WhatsApp' ></Button>
               </div>
             </div>
           </div>
