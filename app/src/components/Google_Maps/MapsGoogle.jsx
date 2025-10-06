6b18230fbecd6d3cd8cb41a819eb930bde211b98
Gabriel Dossa <gabrielvinces2@gmail.com>
2025-10-06 14:53:14 -0300

    Fixing google API

--- app/src/components/Google_Maps/MapsGoogle.jsx
+++ app/src/components/Google_Maps/MapsGoogle.jsx
@@ -3,7 +3,7 @@ import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 export default function MapCardGoogle({ local }) {
   return (
     <div style={{ width: "100%", height: "400px" }}>
-      <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+      <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
         <Map
           center={local}
           zoom={14}
