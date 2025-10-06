76bb1244d356a8b105a5460b9094184ab1d75cd2
Gabriel Dossa <gabrielvinces2@gmail.com>
2025-10-06 13:26:11 -0300

    Add section style

--- app/src/components/SectionTitle/SectionTitle.jsx
+++ app/src/components/SectionTitle/SectionTitle.jsx
@@ -4,6 +4,7 @@ export default function SectionTitle({SectionTitle}){
     return(
         <div className='titleContainer'>
             <h2 >{SectionTitle}</h2>
+            <div className="decoration_underline"></div>
         </div>
     )
 }
