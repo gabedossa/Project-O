a0f91f234e24dc26a2a188a9b24135bdcbc8a793
Gabriel Dossa <gabrielvinces2@gmail.com>
2025-10-06 14:40:37 -0300

    working on site web map

--- app/src/components/Google_Maps/MapsGoogle.jsx
+++ app/src/components/Google_Maps/MapsGoogle.jsx
@@ -1,17 +1,17 @@
 import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 
-export default function MapCardGoogle({local}) {
-
+export default function MapCardGoogle({ local }) {
   return (
-        <div className="mapaLocal">
-          <APIProvider apiKey="AIzaSyAH3VcxH1F-NS--kxRkhNzp48Tg1tYKa_o">
-            <Map 
-            center={local} 
-            zoom={10} 
-            style={{ width: '100%', height: '100%'}}>
-                <Marker position={local} />
-            </Map>
-          </APIProvider>
-        </div>
+    <div style={{ width: "100%", height: "400px" }}>
+      <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+        <Map
+          center={local}
+          zoom={14}
+          style={{ width: "100%", height: "100%" }}
+        >
+          <Marker position={local} />
+        </Map>
+      </APIProvider>
+    </div>
   );
 }
--- app/src/components/MapCard/MapCard.jsx
+++ app/src/components/MapCard/MapCard.jsx
@@ -1,22 +1,24 @@
 import MapCardGoogle from "../Google_Maps/MapsGoogle";
-import"./MapCard.css";
+import "./MapCard.css";
+
 export default function MapCard() {
-  const Cuiaba = { lat: -33.8688, lng: 151.2195  }
-  const Caceres = { lat: -33.8688, lng: 151.2195  }
+  const Cuiaba = { lat: -15.5989, lng: -56.0949 };
+  const Caceres = { lat: -16.0732, lng: -57.6889 };
+
   return (
     <div className="mapCard">
-        <div className="mapaLocal">
-          <div className="LocalName">
-            <p>Cuiabá</p>
-          </div>
-          <MapCardGoogle local={Cuiaba}></MapCardGoogle>
+      <div className="mapaLocal">
+        <div className="LocalName">
+          <p>Cuiabá</p>
         </div>
-        <div className="mapaLocal">
-          <div className="LocalName">
-            <p>Cáceres</p>
-          </div>
-          <MapCardGoogle local={Caceres}></MapCardGoogle>
-        </div>
+        <MapCardGoogle local={Cuiaba} />
       </div>
+      <div className="mapaLocal">
+        <div className="LocalName">
+          <p>Cáceres</p>
+        </div>
+        <MapCardGoogle local={Caceres} />
+      </div>
+    </div>
   );
 }
